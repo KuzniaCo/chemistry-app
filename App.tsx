9ffd0983dd443f195e551552c43e0209801bfae0
Julia Komorowska <komolcia@gmail.com>
2022-04-21 16:19:58 +0200

    Added ui-kitten, Checked if it works in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import {ApplicationProvider,Layout, Text } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return(
+  <ApplicationProvider {...eva} theme={eva.light}> 
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to UI Kitten HiHI</Text>
+    </Layout>
+    <Layout style={styles.container} level='1'>
+
+    <Text style={styles.text} status='primary'>Working</Text>
+    <Text style={styles.text} status='success'>Success</Text>
+    <Text style={styles.text} status='info'>Info</Text>
+    <Text style={styles.text} status='warning'>Warning</Text>
+    <Text style={styles.text} status='danger'>Danger</Text>
+    <Text style={styles.text} status='basic'>Basic</Text>
+    <View style={styles.controlContainer}>
+      <Text style={styles.text} status='control'>Control</Text>
     </View>
-  );
-}
+
+  </Layout>
+  </ApplicationProvider>//the application provider has to be in code
+);
+  }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    margin: 4,
+  },
+  controlContainer: {
+    borderRadius: 4,
+    margin: 4,
+    padding: 4,
+    backgroundColor: '#3366FF',
   },
 });
