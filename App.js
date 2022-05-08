006ad64d0cce7744c4cfb5e46f55089df42854e4
Bartosz <bar.brodowski@gmail.com>
2022-05-08 14:08:02 +0200

    Adding carousel

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import MainView from "./src/ui/components/MainView";
 
 const App = () => {
     return (
         <View>
-            <StatusBar />
             <MainView />
         </View>
     );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
