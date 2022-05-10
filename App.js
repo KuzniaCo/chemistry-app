32c519fee7cd2f31661fd13be7b1278b22d2d47e
komolcia <58554458+komolcia@users.noreply.github.com>
2022-05-10 18:26:47 +0200

    Added Settings View (#40)

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View,Button } from 'react-native'
 import React from 'react'
+import SettingsView from './src/SettingsView.js'
+import  { Component, PropTypes } from 'react';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'; 
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+class HomeScreen extends React.Component {  
+  render() {  
+      return (  
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>  
+              <Text>Home Screen</Text>  
+              <Button  
+                  title="Settings"  
+                  onPress={() => this.props.navigation.navigate('Settings')}  
+              />  
+          </View>  
+      );  
+  }  
+}  
 
-export default App
+const AppNavigator = createStackNavigator(  
+  {  
+      Settings: SettingsView,  
+      Home: HomeScreen
+  },  
+  {  
+      initialRouteName: "Home"  
+  }  
+);  
 
-const styles = StyleSheet.create({})
-
+const AppContainer = createAppContainer(AppNavigator);  
+export default class App extends React.Component {  
+  render() {  
+      return <AppContainer />;  
+  }  
+}  
