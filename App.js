c372204f70ba15ade207bf964493be760fee526a
Julia Komorowska <komolcia@gmail.com>
2022-05-13 11:38:43 +0200

    Cleared App.js

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Settings } from "react-native-web";
-import SettingsView from "./src/SettingsView";
 
 const App = () => {
     return (
