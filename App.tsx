bbf022f8f07f3bed978eaee86ab861914d4a2893
Bartosz <bar.brodowski@gmail.com>
2022-05-15 19:03:04 +0200

    Cleaning up App.js, adding proper folder structure

--- App.tsx
+++ App.tsx
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './components/Login';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      <Login />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <View>
+            <Text>App</Text>
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
