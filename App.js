4bcd82181236b41d3581f3f1e26fd73f28c0b572
Bartosz <bar.brodowski@gmail.com>
2022-05-15 19:27:41 +0200

    Main View, TODO add calendar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import MainView from "./src/ui/components/MainView";
 
 const App = () => {
     return (
         <View>
-            <Text>App</Text>
+            <MainView />
         </View>
     );
 };
