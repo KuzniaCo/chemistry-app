8ccc08ff704b484bc1ca946dd2b85677a1e5f547
Miłosz Baca <mbacaa@icloud.com>
2022-05-22 17:53:38 +0200

    added level view

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const App = () => {
-    return (
-        <View>
-            <Text>App</Text>
-        </View>
-    );
+	return (
+		<View>
+			<Text>App</Text>
+		</View>
+	);
 };
 
 export default App;
-
